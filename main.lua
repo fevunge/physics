efc82592f6afe010d7c5be93ada00e5b1bd95977
fevunge <developervunge@gmail.com>
2025-12-27 16:34:17 +0100

    feat: add energy-based jump and horizontal movement with right/left keys

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ end
 
 function love.load()
 	rect = {}
+	rect.energy = 2000
 	rect.x = 100
 	rect.y = 0
 	rect.width = 50
@@ -23,21 +24,43 @@ function love.update(delta)
 	end
 	if (rect.y + rect.height) < love.graphics.getHeight() - 50 then
 		rect.y = rect.y + (9.8 * delta * FPS)
-	end
-	if love.keyboard.isDown("w") then
-		rect.y = rect.y - rect.height / 2
-	end
-	if love.keyboard.isDown("a") then
-		rect.velocity = rect.velocity + 0.1
-	end
-	if love.keyboard.isDown("d") then
-		rect.velocity = rect.velocity - 0.1
+	elseif love.keyboard.isDown("w") then
+		if rect.energy > 500 then
+			print("jump")
+			rect.y = rect.y - rect.height * (rect.energy / 500)
+			if rect.energy > 55 then
+				rect.energy = rect.energy - 55
+			else
+				rect.energy = 0
+			end
+		end
 	end
 	if love.keyboard.isDown("right") then
-		rect.x = rect.x + rect.velocity * delta * FPS
+		rect.x = rect.x + (rect.velocity * (rect.energy / 1000)) * delta * FPS
+		if rect.energy > 5 then
+			rect.energy = rect.energy - (5 + (rect.velocity / 100))
+		else
+			rect.energy = 0
+		end
 	elseif love.keyboard.isDown("left") then
-		rect.x = rect.x - rect.velocity * delta * FPS
+		rect.x = rect.x - (rect.velocity * (rect.energy / 1000)) * delta * FPS
+		if rect.energy > 5 then
+			rect.energy = rect.energy - (5 + (rect.velocity / 100))
+		else
+			rect.energy = 0
+		end
 	end
+	if love.keyboard.isDown("a") then
+		if rect.velocity < 500 then
+			rect.velocity = rect.velocity + 0.1
+		end
+	end
+	if love.keyboard.isDown("d") then
+		if rect.velocity > 0 then
+			rect.velocity = rect.velocity - 0.1
+		end
+	end
+	rect.energy = rect.energy + 2.5
 end
 
 function love.mousepressed(x, y, button, istouch)
@@ -48,7 +71,8 @@ function love.mousepressed(x, y, button, istouch)
 end
 
 function love.draw()
-	love.graphics.print(("velocity %.2f"):format(rect.velocity), 20, 20)
+	love.graphics.print(("velocity %.2f px/s"):format(rect.velocity), 20, 20)
+	love.graphics.print(("energy %.2f J"):format(rect.energy), 20, 40)
 	paint.apply(0.8, 0.2, 0.2)
 	love.graphics.rectangle("fill", rect.x, rect.y, rect.width, rect.height)
 	ground()
